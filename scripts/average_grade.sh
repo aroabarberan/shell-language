1672151ff991ddec2b88e163cf4fc98db6b210dd
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-04-15 19:13:30 +0200

    exercises: average grade

---
+++ scripts/average_grade.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clear
+
+echo -n 'Enter age: ';
+read age;
+
+echo -n 'Enter course (first/second): ';
+read course;
+
+if [ $course == 'first' ]
+then
+	echo 'Enter 3 notes with decimals:';
+	echo -n 'Note del primer quarter of a first: ';
+	read note1;
+	echo -n 'Note del second quarter of a first: ';
+	read note2;
+	echo -n 'Note del tercer  quarter of a first: ';
+	read note3;
+	
+	sum=$(echo "scale=2; $note1 + $note2 + $note3" | bc -l);
+	average=$(echo "scale=2; $sum / 3" | bc -l);
+	
+	echo 'Your average rating is' $average 'points';
+	echo 'If you are a worker, you can finish your studies with ' $(($age+1)) 'years';
+fi
+
+if [ $course == 'second' ]
+then
+	echo 'Enter 2 notes with decimals:';
+	echo -n 'Note of the first quarter of a second: ';
+	read note1;
+	echo -n 'Note of the second quarter of a second:';
+	read note2;
+	
+	sum=$(echo "scale=2; $note1 + $note2" | bc -l);
+	average=$(echo "scale=2; $sum / 2" | bc -l);
+	
+	echo 'Your average rating is' $average 'points';
+	echo 'If you are a worker, you can finish your studies with' $age 'anios';
+fi
+exit 0
