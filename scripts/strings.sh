5758ba7f6de3b4c170a614d21ffa3c011d3f9929
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-04-17 00:04:23 +0200

    exercises: test of collecting data by console with quotes and without quotes

---
+++ scripts/strings.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Tema 4 parte 3 ejercicio 
+
+clear
+
+echo "Give me a whole number:";
+read variable1;
+
+echo "Give me a real number:";
+read variable2;
+
+echo "Give me a string without quotes and without blank spaces";
+read variable3;
+
+echo "Give me a string without quotes and with blank spaces";
+read variable4;
+
+echo "Give me a string with double quotes and no white space";
+read variable5;
+
+echo "Give me a string with double quotes and with blank spaces";
+read variable6;
+
+echo "Give me a string with single quotes and no white space";
+read variable7;
+
+echo "Give me a string with single quotes and with blank spaces";
+read variable8;
+
+echo $ variable1;
+echo $ variable2;
+echo $ variable3;
+echo $ variable4;
+echo $ variable5;
+echo $ variable6;
+echo $ variable7;
+echo $ variable8;
+
+echo "Give me a string with three words separated by spaces:";
+read variable9 variables10 variable11;
+
+echo "Give me a string with three words separated by spaces:";
+read variable12 variables13;
+
+echo "Give me a string with three words separated by spaces:";
+read variable14;
+
+echo $ variable9;
+echo $ variable10;
+echo $ variable11;
+echo $ variable12;
+echo $ variable13;
+echo $ variable14;
+
+exit 0
