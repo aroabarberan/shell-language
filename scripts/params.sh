b43a4d87770b93c2851c8f08f4f859df0a3cd561
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-04-17 00:43:06 +0200

    exercises: params

---
+++ scripts/params.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Tema 4 parte 3 ejercicio 
+
+clear
+
+echo "You have executed the SH with name: $ 0";
+echo "The number of parameters is: $ #";
+echo "The parameters are: $ *";
+echo "Your first parameter is: $ 1";
+echo "Your second parameter is: $ 2";
+echo "Your third parameter is: $ 3";
+
+exit 0
