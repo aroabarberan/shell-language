0fd036e6bfebccc73aab1409a36a7a33e925f5c5
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-04-18 02:13:58 +0200

    fixed: eliminate white space between $ and the variable

--- scripts/check_directories.sh
+++ scripts/check_directories.sh
@@ -5,10 +5,10 @@
 # Next it will show what the user has entered and
 # will finally show one of the following messages to the user:
 
-echo "$ dir1 and $ dir2 are directories."
-echo "$ dir1 is a directory, but $ dir2 does not."
-echo "$ dir2 is a directory, but $ dir1 does not."
-echo "Neither $ dir1 nor $ dir2 are directories."
+echo "$dir1 and $dir2 are directories."
+echo "$dir1 is a directory, but $dir2 does not."
+echo "$dir2 is a directory, but $dir1 does not."
+echo "Neither $dir1 nor $dir2 are directories."
 
 clear
 
@@ -18,17 +18,17 @@ read dir1;
 echo -n "Enter the name of another directory:";
 read dir2;
 
-if [-d $ dir1] && [-d $ dir2]
+if [-d $dir1] && [-d $dir2]
 then
     echo "$ dir1 and $ dir2 are directories";
-elif [-d $ dir1] && [! -d $ dir2]
+elif [-d $dir1] && [! -d $dir2]
 then
     echo "$ dir1 is a directory, but $ dir2 no.";
-elif [! -d $ dir1] && [-d $ dir2]
+elif [! -d $dir1] && [-d $dir2]
 then
-    echo "$ dir2 is a directory, but $ dir1 no.";
+    echo "$dir2 is a directory, but $dir1 no.";
 else
-    echo "Neither $ dir1 nor $ dir2 are directories.";
+    echo "Neither $dir1 nor $dir2 are directories.";
 fi
 
 exit 0
